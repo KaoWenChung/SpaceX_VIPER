2222a0c1e992a57c83402851e282170b90d9fe3f
wyn <k022298@gmail.com>
2023-03-01 17:21:59 +0000

    Added check/ cross icon in the launch cell

--- SpaceX_VIPER/Presentation/SpaceXScene/View/Cell/LaunchTableViewCell.swift
+++ SpaceX_VIPER/Presentation/SpaceXScene/View/Cell/LaunchTableViewCell.swift
@@ -10,6 +10,7 @@ final class LaunchTableViewCell: UITableViewCell {
     @IBOutlet weak private(set) var dateLabel: UILabel!
     @IBOutlet weak private(set) var rocketLabel: UILabel!
     @IBOutlet weak private(set) var daysLabel: UILabel!
+    @IBOutlet weak private(set) var isSuccessImage: UIImageView!
     private var imageLoadTask: CancellableType?
 
     override func prepareForReuse() {
@@ -31,5 +32,10 @@ final class LaunchTableViewCell: UITableViewCell {
             await missionImageView.downloaded(imageLoader: interactor.imageRepository, from: interactor.imageURL)
         }
         imageLoadTask = task
+        if let isSuccess = interactor.isSuccess {
+            let image = isSuccess == true ? "check" : "cross"
+            isSuccessImage.image = UIImage(named: image)
+        }
+        isSuccessImage.isHidden = interactor.isSuccess == nil
     }
 }
